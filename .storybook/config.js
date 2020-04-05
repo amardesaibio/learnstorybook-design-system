c205d5d94b8f483e9e4ee11529bee265cd44e895
Amar Desai <amardesai16@gmail.com>
2020-04-05 15:25:40 -0400

    Changelog for v0.1.0 [skip ci]

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { configure, addDecorator } from '@storybook/react';
-//import { withA11y } from '@storybook/addon-a11y';
+import { withA11y } from '@storybook/addon-a11y';
 //import 'storybook-chromatic';
 
 import { GlobalStyle } from '../src/shared/global';
 
-//addDecorator(withA11y);
+addDecorator(withA11y);
 addDecorator(story => (
   <>
     <GlobalStyle />
